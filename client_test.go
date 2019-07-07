427e8b87627773124ffb973b778185ee86c5060e
yujc <yujc@youshu.cc>
2019-07-07 23:11:22 +0800

    支持http server

--- client_test.go
+++ client_test.go
@@ -2,25 +2,48 @@ package main
 
 import (
 	"github.com/hprose/hprose-golang/rpc"
-	"sample/service"
+	"sample/util"
 	"testing"
 )
+
+//stub
+type clientStub struct {
+	Hello       func(string) string
+	GetUserInfo func(uid int64) util.State
+}
 
 func GetClient() *rpc.TCPClient {
 	return rpc.NewTCPClient("tcp4://127.0.0.1:8050")
 }
 
+//测试服务里的方法
 func TestSampleService_GetUserInfo(t *testing.T) {
 	client := GetClient()
 
 	defer client.Close()
-	var comment service.SampleService
-	client.UseService(&comment, "Sample")
+	var stub clientStub
+	client.UseService(&stub, "Sample") //使用命名空间
 
-	rep := comment.GetUserInfo(10001)
+	rep := stub.GetUserInfo(10001)
 	if rep.ErrCode > 0 {
 		t.Error(rep.ErrMsg)
 	} else {
 		t.Log(rep.Data)
+	}
+}
+
+//测试普通方法
+func TestHello(t *testing.T) {
+	client := GetClient()
+
+	defer client.Close()
+	var stub clientStub
+	client.UseService(&stub)
+
+	rep := stub.Hello("func")
+	if rep != "" {
+		t.Error(rep)
+	} else {
+		t.Log(rep)
 	}
 }
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"github.com/hprose/hprose-golang/rpc"
+	"net/http"
 	"sample/service"
 	"sample/util"
 )
@@ -15,6 +16,7 @@ func hello(name string) string {
 func main() {
 
 	configFile := flag.String("c", "config/rd.ini", "config file")
+	protocol := flag.String("P", "tcp", "the protocol, such as tcp,http")
 	flag.Parse()
 
 	err := util.InitConfig(*configFile)
@@ -23,17 +25,38 @@ func main() {
 		return
 	}
 
-	fmt.Printf("tcp server is running at %s\n", util.Cfg.ListenAddr)
-	server := rpc.NewTCPServer("tcp4://" + util.Cfg.ListenAddr + "/")
+	fmt.Printf("server is running at %s\n", util.Cfg.ListenAddr)
 
-	//注册func
-	server.AddFunction("hello", hello)
+	if *protocol == "tcp" {
+		server := rpc.NewTCPServer("tcp4://" + util.Cfg.ListenAddr + "/")
 
-	//注册struct，命名空间是SampleService
-	server.AddInstanceMethods(&service.SampleService{}, rpc.Options{NameSpace: "Sample"})
-	err = server.Start()
-	if err != nil {
-		fmt.Printf("start server fail, err:%v\n", err)
+		//注册func
+		server.AddFunction("hello", hello)
+
+		//注册struct，命名空间是SampleService
+		server.AddInstanceMethods(&service.SampleService{}, rpc.Options{NameSpace: "Sample"})
+		err = server.Start()
+		if err != nil {
+			fmt.Printf("start server fail, err:%v\n", err)
+			return
+		}
+	} else if *protocol == "http" {
+		server := rpc.NewHTTPService()
+
+		//注册func
+		server.AddFunction("hello", hello)
+
+		//注册struct，命名空间是SampleService
+		server.AddInstanceMethods(&service.SampleService{}, rpc.Options{NameSpace: "Sample"})
+
+		err = http.ListenAndServe(util.Cfg.ListenAddr, server)
+		if err != nil {
+			fmt.Printf("start server fail, err:%v\n", err)
+			return
+		}
+	} else {
+		fmt.Printf("err protocol config : %v\n", *protocol)
 		return
 	}
+
 }
